e7db03be88f5ceb1f434a73d7213a5a79255dfa0
janxkoci <janxkoci@gmail.com>
2025-07-13 21:21:04 +0000

    Deploying to gh-pages from @ flegontovlab/flegontovlab.github.io@76f5bbe995bfda922df8e5c29acc948f54971528 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -430,51 +430,21 @@ ninja.data = [{
           section: "News",},{id: "news-our-latest-paper-was-recently-published-in-nature-sparkles-smile-see-the-press-release-in-czech",
           title: 'Our latest paper was recently published in Nature. :sparkles: :smile: See the press...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-the-indo-european-and-african-puzzles",
+          title: 'The Indo-European and African Puzzles',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-unraveling-the-genetic-tapestry-of-southeast-asia",
+          title: 'Unraveling the Genetic Tapestry of Southeast Asia',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-sharpening-the-tools",
+          title: 'Sharpening the Tools',
+          description: "Innovations in Computational Genomics",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
